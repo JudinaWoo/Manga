c3a50b61996cdad87a69a9684f74d78d73d24582
Zhiting Wu <zhiting.wu@cdiscount.com>
2021-10-27 14:44:27 +0200

    feat: 🎸 add moviesSlice & store

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { Provider as ReduxProvider } from "react-redux";
 
 import HomePage from "../home/HomePage";
 import SearchPage from "../search/SearchPage";
@@ -8,24 +9,27 @@ import MyListPage from "../my-list/MyListPage";
 
 import { homePath, myListPath, searchPath } from "../routes";
 import theme from "../ds/theme";
+import store from "./store";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Switch>
-          <Route exact path={homePath}>
-            <HomePage />
-          </Route>
-          <Route path={searchPath}>
-            <SearchPage />
-          </Route>
-          <Route path={myListPath}>
-            <MyListPage />
-          </Route>
-        </Switch>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Switch>
+            <Route exact path={homePath}>
+              <HomePage />
+            </Route>
+            <Route path={searchPath}>
+              <SearchPage />
+            </Route>
+            <Route path={myListPath}>
+              <MyListPage />
+            </Route>
+          </Switch>
+        </ThemeProvider>
+      </BrowserRouter>
+    </ReduxProvider>
   );
 }
--- src/home/HomePage/index.js
+++ src/home/HomePage/index.js
@@ -1,9 +1,13 @@
+import { Alert } from "@material-ui/lab";
 import { v4 as uuid } from "uuid";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import BaseLayout from "../../ds/BaseLayout";
 import MovieCard from "../../ds/MovieCard";
+
+import { emptyMovies, fillMovies } from "../../features/movies/moviesSlice";
 
 const defaultMovies = new Array(5).fill(null).map(() => ({
   id: uuid(),
@@ -12,16 +16,28 @@ const defaultMovies = new Array(5).fill(null).map(() => ({
 }));
 
 export default function HomePage() {
-  const [movies] = React.useState(defaultMovies);
+  // const [movies] = React.useState(defaultMovies);
+  const movies = useSelector((state) => state.movies);
+  const hasMovies = movies.length > 0;
+  const dispatch = useDispatch();
+
+  const fill = () => dispatch(fillMovies(defaultMovies));
+  const empty = () => dispatch(emptyMovies());
 
   return (
     <BaseLayout>
       <Grid container spacing={3}>
-        {movies.map((movie) => (
-          <Grid item key={movie.id}>
-            <MovieCard {...movie} />
-          </Grid>
-        ))}
+        {hasMovies ? (
+          movies.map((movie) => (
+            <Grid item key={movie.id}>
+              <MovieCard {...movie} />
+            </Grid>
+          ))
+        ) : (
+          <Alert severity="info">This is an info alert — check it out!</Alert>
+        )}
+        <button onClick={fill}>FILL</button>
+        <button onClick={empty}>EMPTY</button>
       </Grid>
     </BaseLayout>
   );
